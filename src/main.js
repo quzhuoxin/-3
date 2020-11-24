f1ee5280a55db96f03775673e1dbab168b7d2355
quzhuoxin <quzhuoxin@foxmail.com>
2020-11-24 15:16:12 +0800

    真vue脚手架3

--- src/main.js
+++ src/main.js
@@ -2,8 +2,13 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import Axios from 'axios'
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
 
+Vue.prototype.$http=Axios
 Vue.config.productionTip = false
+Vue.use(ElementUI);
 
 new Vue({
   router,
